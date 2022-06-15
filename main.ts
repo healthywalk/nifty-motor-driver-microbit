2cd8d650b426b6f077bbe113f89f3945d74b68f6
healthywalk <89001945+healthywalk@users.noreply.github.com>
2022-06-15 14:38:59 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -27,7 +27,8 @@ namespace NiftyMotorDriver {
     let motor22a: AnalogPin = degitalPin2analogPin(motor22)
 
     //% blockId=initializeMotorDriver
-    //% block="initialize Driver motor=$motor pin1=$pin1 pin2=$pin2"
+    //% block="initialize Driver $motor pin1=$pin1 pin2=$pin2"
+    //% motor=m1,m2
     export function initializeMotorDriver(motor: MotorEnum, pin1: DigitalPin, pin2: DigitalPin): void {
         if(motor == MotorEnum.m1) {
             motor11 = pin1
